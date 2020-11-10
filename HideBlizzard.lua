d4848c150299085209b4ef4d0009f826f1c2c4dc
KevinSK <shinozakisama@live.com>
2020-11-10 23:27:04 +0800

    Update HideBlizzard.lua

--- HideBlizzard.lua
+++ HideBlizzard.lua
@@ -1,43 +1,58 @@
 local _, Cell = ...
 local F = Cell.funcs
 
-local empty = function() end
-local hider = CreateFrame("Frame", nil, nil, "SecureFrameTemplate")
-hider:Hide()
+-- stolen from elvui
+local hiddenParent = CreateFrame("Frame", nil, _G.UIParent)
+hiddenParent:SetAllPoints()
+hiddenParent:Hide()
 
-local function HideBlizzardFrame(frame)
-	if not frame then return end
+local function HideFrame(baseName, doNotReparent)
+    local frame = _G[baseName]
 
-	frame:UnregisterAllEvents()
-	frame:SetScript("OnEvent", nil)
-	frame:SetScript("OnUpdate", nil)
-	frame:SetScript("OnSizeChanged", nil)
-	frame:EnableMouse(false)
-	frame:EnableKeyboard(false)
-    frame:Hide()
-    frame:SetParent(hider)
-	frame:SetAlpha(0)
-    frame.Show = empty
-	RegisterStateDriver(frame, "visibility", "hide")
+    if frame then
+        frame:UnregisterAllEvents()
+        frame:Hide()
+
+        if not doNotReparent then
+            frame:SetParent(hiddenParent)
+        end
+
+        local health = frame.healthBar or frame.healthbar
+        if health then
+            health:UnregisterAllEvents()
+        end
+
+        local power = frame.manabar
+        if power then
+            power:UnregisterAllEvents()
+        end
+
+        local spell = frame.castBar or frame.spellbar
+        if spell then
+            spell:UnregisterAllEvents()
+        end
+
+        local altpowerbar = frame.powerBarAlt
+        if altpowerbar then
+            altpowerbar:UnregisterAllEvents()
+        end
+
+        local buffFrame = frame.BuffFrame
+        if buffFrame then
+            buffFrame:UnregisterAllEvents()
+        end
+    end
 end
 
 function F:HideBlizzard()
-    if (not CompactRaidFrameManager) then return end
+    -- raid
+    CompactRaidFrameManager_SetSetting("IsShown", "0")
+    _G.UIParent:UnregisterEvent("GROUP_ROSTER_UPDATE")
+    _G.CompactRaidFrameManager:UnregisterAllEvents()
+    _G.CompactRaidFrameManager:SetParent(hiddenParent)
 
-    HideBlizzardFrame(CompactRaidFrameManager)
-    HideBlizzardFrame(CompactRaidFrameContainer)
-    UIParent:UnregisterEvent("GROUP_ROSTER_UPDATE")
-
-    if CompactUnitFrameProfiles then CompactUnitFrameProfiles:UnregisterAllEvents() end
-
-    -- hide party frames
+    -- party
     for i = 1, 4 do
-        local frame = _G["PartyMemberFrame"..i]
-        frame:SetParent(hider)
-        frame:Hide()
-        frame:UnregisterAllEvents()
-        frame.Show = empty
-        _G["PartyMemberFrame"..i..'HealthBar']:UnregisterAllEvents()
-        _G["PartyMemberFrame"..i..'ManaBar']:UnregisterAllEvents()
+        HideFrame("PartyMemberFrame"..i)
     end
 end
