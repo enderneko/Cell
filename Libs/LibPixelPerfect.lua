3e60f90d868dcdc7f5a8c1afa51777cfc62f2b8e
KevinSK <shinozakisama@live.com>
2021-04-12 12:56:56 +0800

    fix scaling issue after resolution changed

--- Libs/LibPixelPerfect.lua
+++ Libs/LibPixelPerfect.lua
@@ -4,55 +4,49 @@
 -- http://wow.gamepedia.com/UI_Scale
 -- http://www.wowinterface.com/forums/showthread.php?t=31813
 --------------------------------------------
-local lib = LibStub:NewLibrary("LibPixelPerfect", "2")
+local lib = LibStub:NewLibrary("LibPixelPerfect", "3")
 if not lib then return end
 
 function lib:GetResolution()
-	-- local res = select(GetCurrentResolution(), GetScreenResolutions())
-	-- local hRes, vRes = string.split("x", res)
-	if GetCurrentResolution() == 0 then
-		-- windowed mode before 8.0, or maybe something goes wrong?
-		return
-	else
-		return string.match(({GetScreenResolutions()})[GetCurrentResolution()], "(%d+)x(%d+)")
-	end
+    -- return string.match(({GetScreenResolutions()})[GetCurrentResolution()], "(%d+)x(%d+)")
+    return GetPhysicalScreenSize()
 end
 
 -- The UI Scale goes from 1 to 0.64. 
 -- At 768y we see pixel-per-pixel accurate representation of our texture, 
 -- and again at 1200y if at 0.64 scale.
 function lib:GetPixelPerfectScale()
-	local hRes, vRes = lib:GetResolution()
-	if vRes then
-		return 768/vRes
-	else -- windowed mode before 8.0, or maybe something goes wrong?
-		return 1
-	end
+    local hRes, vRes = lib:GetResolution()
+    if vRes then
+        return 768/vRes
+    else -- windowed mode before 8.0, or maybe something goes wrong?
+        return 1
+    end
 end
 
 -- scale perfect!
 function lib:PixelPerfectScale(frame)
-	frame:SetScale(lib:GetPixelPerfectScale())
+    frame:SetScale(lib:GetPixelPerfectScale())
 end
 
 -- position perfect!
 function lib:PixelPerfectPoint(frame)
-	local left = frame:GetLeft()
-	local top = frame:GetTop()
-	
-	frame:ClearAllPoints()
-	frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", math.floor(left + .5), math.floor(top + .5))
+    local left = frame:GetLeft()
+    local top = frame:GetTop()
+    
+    frame:ClearAllPoints()
+    frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", math.floor(left + .5), math.floor(top + .5))
 end
 
 function lib:SavePixelPerfectPosition(frame, positionTable)
-	local left = math.floor(frame:GetLeft() + .5)
-	local top = math.floor(frame:GetTop() + .5)
-	positionTable[1], positionTable[2] = left, top
+    local left = math.floor(frame:GetLeft() + .5)
+    local top = math.floor(frame:GetTop() + .5)
+    positionTable[1], positionTable[2] = left, top
 end
 
 function lib:LoadPixelPerfectPosition(frame, positionTable)
-	if type(positionTable) ~= "table" or #positionTable ~= 2 then return end
+    if type(positionTable) ~= "table" or #positionTable ~= 2 then return end
 
-	frame:ClearAllPoints()
-	frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", positionTable[1], positionTable[2])
+    frame:ClearAllPoints()
+    frame:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", positionTable[1], positionTable[2])
 end
