3099eb4ca6f0a115cdf6455e18cc293c8222a269
KevinSK <shinozakisama@live.com>
2021-07-13 17:36:22 +0800

    update changelog

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -42,6 +42,12 @@ select(2, ...).L = setmetatable({
     ["marksTips"] = "\n|rTarget marker\nLeft-Click: |cffffffffset raid marker on target|r\nRight-Click: |cfffffffflock raid marker on target (in your group)|r",
 
     ["CHANGE LOGS"] = [[
+        <h1>r55-release (Jul 13, 2021, 17:35 GMT+8)</h1>
+        <p>* Updated RaidDebuffs (Tazavesh).</p>
+        <p>* Updated BigDebuffs (tormented affix related).</p>
+        <p>* Fixed button backdrop in options frame.</p>
+        <br/>
+
         <h1>r54-release (Jul 9, 2021, 01:49 GMT+8)</h1>
         <p>* Fixed BattleRes timer.</p>
         <br/>
