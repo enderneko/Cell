b044eb25e9136b80853d0b9a5608b456600b55f1
KevinSK <shinozakisama@live.com>
2021-07-21 12:36:49 +0800

    update utils: utf8sub

--- Utils.lua
+++ Utils.lua
@@ -69,6 +69,39 @@ function F:SplitToNumber(sep, str)
     return unpack(ret)
 end
 
+local function Chsize(char)
+    if not char then
+        return 0
+    elseif char > 240 then
+        return 4
+    elseif char > 225 then
+        return 3
+    elseif char > 192 then
+        return 2
+    else
+        return 1
+    end
+end
+
+function F:Utf8sub(str, startChar, numChars)
+    if not str then return "" end
+    local startIndex = 1
+    while startChar > 1 do
+        local char = string.byte(str, startIndex)
+        startIndex = startIndex + Chsize(char)
+        startChar = startChar - 1
+    end
+    
+    local currentIndex = startIndex
+    
+    while numChars > 0 and currentIndex <= #str do
+        local char = string.byte(str, currentIndex)
+        currentIndex = currentIndex + Chsize(char)
+        numChars = numChars -1
+    end
+    return str:sub(startIndex, currentIndex - 1)
+end
+
 -------------------------------------------------
 -- table
 -------------------------------------------------
