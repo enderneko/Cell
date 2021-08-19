d5449138fd13d9aa956362503bad10cb2e004a2c
KevinSK <shinozakisama@live.com>
2021-08-20 06:05:05 +0800

    update changelog

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -42,6 +42,12 @@ select(2, ...).L = setmetatable({
     ["marksTips"] = "\n|rTarget marker\nLeft-Click: |cffffffffset raid marker on target|r\nRight-Click: |cfffffffflock raid marker on target (in your group)|r",
 
     ["CHANGE LOGS"] = [[
+        <h1>r62-release (Aug 20, 2021, 06:05 GMT+8)</h1>
+        <p>+ Added a Rename button for indicators.</p>
+        <p>* Fixed Layout Auto Switch (battleground &amp; arena).</p>
+        <p>* Updated zhTW.</p>
+        <br/>
+
         <h1>r61-release (Aug 16, 2021, 22:30 GMT+8)</h1>
         <p>+ New Indicator: Aggro (border).</p>
         <p>* Renamed Indicators: Aggro Indicator -> Aggro (blink), Aggro Bar -> Aggro (bar).</p>
