adb1e8e31debb152c94de1a825938366ff7df478
KevinSK <shinozakisama@live.com>
2021-11-18 09:22:10 +0800

    update changelog

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -42,6 +42,11 @@ select(2, ...).L = setmetatable({
     ["marksTips"] = "\n|rTarget marker\nLeft-Click: |cffffffffset raid marker on target|r\nRight-Click: |cfffffffflock raid marker on target (in your group)|r",
 
     ["CHANGE LOGS"] = [[
+        <h1>r70-release (Nov 18, 2021, 09:20 GMT+8)</h1>
+        <p>+ Added several new options in Appearance.</p>
+        <p>* Added "Show Duration" option to custom TEXT indicator.</p>
+        <br/>
+
         <h1>r69-release (Nov 16, 2021, 09:10 GMT+8)</h1>
         <p>+ Added "Background Alpha" in Appearance.</p>
         <p>* Updated Raid Debuffs indicator, it can show up to 3 debuffs now.</p>
