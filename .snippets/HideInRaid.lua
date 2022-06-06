37f07ea8fd9cd2b5b9ed75cf27503786c6b35c4f
enderneko <enderneko-dev@outlook.com>
2022-06-06 20:34:31 +0800

    [snippet] hide in raid

---
+++ .snippets/HideInRaid.lua
@@ -0,0 +1,13 @@
+for i = 1, 8 do
+    local header = _G["CellRaidFrameHeader"..i]
+    header:SetAttribute("showRaid", false)
+end
+
+local function MainFrame_GroupTypeChanged(groupType)
+    if groupType == "raid" then
+        CellMenuFrame:Hide()
+    else
+        CellMenuFrame:Show()
+    end
+end
+Cell:RegisterCallback("GroupTypeChanged", "MainFrame_GroupTypeChanged2", MainFrame_GroupTypeChanged)
