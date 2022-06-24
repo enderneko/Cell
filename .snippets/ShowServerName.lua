a3726ad61a4e1822241abb4d03b8eb591852a629
enderneko <enderneko-dev@outlook.com>
2022-06-24 16:15:25 +0800

    [snippet]server name

---
+++ .snippets/ShowServerName.lua
@@ -0,0 +1,29 @@
+local F = Cell.funcs
+
+local function GetServerName(unit)
+    if not UnitIsPlayer(unit) then return "" end
+
+    local _, serverName = UnitNameUnmodified(unit)
+    if not serverName then
+        serverName = GetNormalizedRealmName()
+    end
+    return serverName
+end
+
+local function CreateServerName(parent)
+    local serverName = parent.indicators.nameText:CreateFontString(nil, "OVERLAY")
+
+    local font = parent.indicators.nameText.name:GetFont()
+    serverName:SetFont(font, 11, "") -- font, size, flags
+    serverName:SetShadowOffset(1, -1)
+    serverName:SetShadowColor(0, 0, 0, 1)
+    serverName:SetPoint("TOP", parent.indicators.nameText, "BOTTOM", 0, -1)
+
+    hooksecurefunc(parent.indicators.nameText, "UpdateName", function()
+        serverName:SetText(GetServerName(parent.state.unit))
+    end)
+end
+
+F:IterateAllUnitButtons(function(b)
+    CreateServerName(b)
+end)
