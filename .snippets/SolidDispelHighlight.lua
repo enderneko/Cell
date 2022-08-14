4fe44edf7ffca1e53377f2c878685ee4cb6ea827
enderneko <enderneko-dev@outlook.com>
2022-08-15 05:14:42 +0800

    [snippet] SolidDispelHighlight

---
+++ .snippets/SolidDispelHighlight.lua
@@ -0,0 +1,37 @@
+-- solid dispel highlight
+-- 使用纯色而非渐变色显示驱散高亮
+local ALPHA = 0.6
+
+local F = Cell.funcs
+
+F:IterateAllUnitButtons(function(b)
+    local dispels = b.indicators.dispels
+
+    dispels.highlight:ClearAllPoints()
+    dispels.highlight:SetPoint("BOTTOMLEFT", b.widget.healthBar)
+    dispels.highlight:SetPoint("BOTTOMRIGHT", b.widget.healthBar)
+    dispels.highlight:SetPoint("TOP", b.widget.healthBar)
+
+    function dispels:SetDispels(dispelTypes)
+        local r, g, b, a = 0, 0, 0, 0
+
+        local i = 1
+        for dispelType, _ in pairs(dispelTypes) do
+            if i == 1 and dispelType then
+                r, g, b, a = DebuffTypeColor[dispelType].r, DebuffTypeColor[dispelType].g, DebuffTypeColor[dispelType].b, 1
+            end
+            dispels[i]:SetDispel(dispelType)
+            i = i + 1
+        end
+
+        -- hide unused
+        for j = i, 4 do
+            dispels[j]:Hide()
+        end
+
+        -- highlight
+        dispels.highlight:SetColorTexture(r, g, b, a ~= 0 and ALPHA or 0)
+    end
+
+    -- b.widget.shieldBar:SetTexture(Cell.vars.texture)
+end)
