a1385009de1024e4e4100c1af5d33933840782d1
enderneko <enderneko-dev@outlook.com>
2022-11-12 10:53:54 +0800

    [snippet] RepointPartyAndArenaPets

---
+++ .snippets/RepointPartyAndArenaPets.lua
@@ -0,0 +1,35 @@
+local point = "TOPLEFT"
+local anchorPoint = "BOTTOMLEFT"
+local x = 0
+local y = -3
+
+local groupPoint = "TOPLEFT"
+local groupAnchorTo = CellMainFrame
+local groupAnchorPoint = "TOPRIGHT"
+local groupX = 100
+local groupY = 0
+
+local func = function() end
+local function Repoint(t, k)
+    local last
+    for i = 1, 5 do
+        local pet = t[k..i]
+        if pet then
+            pet._ClearAllPoints = pet.ClearAllPoints
+            pet.ClearAllPoints = func
+            pet._SetPoint = pet.SetPoint
+            pet.SetPoint = func
+
+            pet:_ClearAllPoints()
+            if last then
+                pet:_SetPoint(point, last, anchorPoint, x, y)
+            else
+                pet:_SetPoint(groupPoint, groupAnchorTo, groupAnchorPoint, groupX, groupY)
+            end
+            last = pet
+        end
+    end
+end
+
+Repoint(Cell.unitButtons.party, "pet")
+Repoint(Cell.unitButtons.arena, "raidpet")
