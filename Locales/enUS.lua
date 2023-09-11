b190ea73396eaf9f46e40eb56900ad3ec3a97bb8
enderneko <enderneko-dev@outlook.com>
2023-09-12 06:12:04 +0800

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -60,7 +60,7 @@ select(2, ...).L = setmetatable({
     ["INVERT"] = "Invert",
     ["Default"] = _G.DEFAULT,
 
-    ["ABOUT"] = "Cell is a unique raid frame addon inspired by CompactRaid and Grid2. Some ideas are from other great addons, such as Aptechka and VuhDo.\nCell is not meant to be a lightweight or powerful raid frame addon. It aims to provide a good user experience.\nHope you enjoy.",
+    ["ABOUT"] = "Cell is a nice raid frame addon inspired by several great addons, such as CompactRaid, Grid2, Aptechka and VuhDo.\nWith a more human-friendly interface, Cell can provide a better user experience, better than ever.\nHope you enjoy.",
     ["RESET"] = "Cell requires a full reset after updating from a very old version",
     ["RESET_CHARACTER"] = "Cell requires a character profile reset after updating from a very old version",
     ["RESET_INCLUDES"] = "Only Click-Castings and Layout Auto Switch are included",
