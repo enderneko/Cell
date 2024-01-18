6f2a3be83c89ed7cbba7ff24510677f99aca0855
enderneko <enderneko-dev@outlook.com>
2024-01-18 12:28:28 +0800

    Fix Quick Assist
    occurred when tweaking filters

--- Utilities/QuickAssist.lua
+++ Utilities/QuickAssist.lua
@@ -546,9 +546,11 @@ end
 
 local function QuickAssist_OnAttributeChanged(self, name, value)
     if name == "unit" then
-        self.unit = value
-        self:RegisterUnitEvent("UNIT_IN_RANGE_UPDATE", value)
-        ResetAuraTables(self)
+        if self.unit ~= value then
+            self.unit = value
+            self:RegisterUnitEvent("UNIT_IN_RANGE_UPDATE", value)
+            ResetAuraTables(self)
+        end
 
         if value then
             Cell.unitButtons.quickAssist.units[value] = self
