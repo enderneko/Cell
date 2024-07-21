c65af82860850c9ef2ace405107fbc6901411f95
honeyhoney <honeyhoney@users.noreply.github.com>
2024-07-21 21:16:09 +0100

    Fix Full Health Color

--- RaidFrames/UnitButton.lua
+++ RaidFrames/UnitButton.lua
@@ -2019,7 +2019,7 @@ local function UnitButton_UpdateHealthMax(self)
         self.widgets.healthBar:SetMinMaxValues(0, self.states.healthMax)
     end
 
-    if Cell.vars.useGradientColor then
+    if Cell.vars.useGradientColor or Cell.vars.useFullColor then
         UnitButton_UpdateHealthColor(self)
     end
 end
@@ -2043,7 +2043,7 @@ local function UnitButton_UpdateHealth(self, diff)
         self.widgets.healthBar:SetBarValue(self.states.health)
     end
 
-    if Cell.vars.useGradientColor then
+    if Cell.vars.useGradientColor or Cell.vars.useFullColor then
         UnitButton_UpdateHealthColor(self)
     end
 
