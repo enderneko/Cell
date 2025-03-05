b5ed9748e989d128a0770eedd45c82b1c0ac1f4b
enderneko <enderneko-dev@outlook.com>
2025-03-05 14:30:24 +0800

    Add "/cell rescale"

--- Core_Wrath.lua
+++ Core_Wrath.lua
@@ -802,6 +802,10 @@ function SlashCmdList.CELL(msg, editbox)
     elseif command == "healers" then
         F.FirstRun()
 
+    elseif command == "rescale" then
+        CellDB["appearance"]["scale"] = P.GetRecommendedScale()
+        ReloadUI()
+
     elseif command == "reset" then
         if rest == "position" then
             Cell.frames.anchorFrame:ClearAllPoints()
@@ -876,6 +880,7 @@ function SlashCmdList.CELL(msg, editbox)
         F.Print(L["Available slash commands"]..":\n"..
             "|cFFFFB5C5/cell options|r, |cFFFFB5C5/cell opt|r: "..L["show Cell options frame"]..".\n"..
             "|cFFFFB5C5/cell healers|r: "..L["create a \"Healers\" indicator"]..".\n"..
+            "|cFFFFB5C5/cell rescale|r: "..strlower(L["Apply Recommended Scale"])..".\n"..
             "|cFFFF7777"..L["These \"reset\" commands below affect all your characters in this account"]..".|r\n"..
             "|cFFFFB5C5/cell reset position|r: "..L["reset Cell position"]..".\n"..
             "|cFFFFB5C5/cell reset layouts|r: "..L["reset all Layouts and Indicators"]..".\n"..
