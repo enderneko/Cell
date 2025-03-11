c80bf869dfca603d1f104a4b6ad89a39ecd1dc8d
enderneko <enderneko-dev@outlook.com>
2025-03-11 11:03:59 +0800

    Fix size of Dispels

--- Indicators/Built-in.lua
+++ Indicators/Built-in.lua
@@ -546,7 +546,6 @@ local function Dispels_UpdateSize(self, iconsShown)
 
     local width, height = self.width, self.height
     if iconsShown then -- SetDispels
-        iconsShown = iconsShown - 1
         if self.orientation == "horizontal"  then
             width = self.width + (iconsShown - 1) * floor(self.width / 2)
             height = self.height
@@ -564,6 +563,8 @@ local function Dispels_UpdateSize(self, iconsShown)
                     width = self.width
                     height = self.height + (i - 1) * floor(self.height / 2)
                 end
+            else
+                break
             end
         end
     end
