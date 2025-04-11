294f21ea58f9a63ee2756754134f4e2e20cb6924
enderneko <enderneko-dev@outlook.com>
2025-04-11 17:12:56 +0800

    Update IndicatorIcons_RemoveBorder.lua

--- .snippets/IndicatorIcons_RemoveBorder.lua
+++ .snippets/IndicatorIcons_RemoveBorder.lua
@@ -1,11 +1,13 @@
 -------------------------------------------------
--- 2024-05-18 03:09:08 GMT+8
+-- 2025-04-11 17:11:18 GMT+8
 -- remove icons border
 -- 删除图标指示器的边框
 -------------------------------------------------
-hooksecurefunc(Cell.iFuncs, "CreateAura_BarIcon", function(name)
-    local f = _G[name]
+local create = Cell.iFuncs.CreateAura_BarIcon
+Cell.iFuncs.CreateAura_BarIcon = function(name, parent)
+    local f = create(name, parent)
     hooksecurefunc(f, "SetCooldown", function()
         f:SetBackdropColor(0, 0, 0, 0)
     end)
-end)
+    return f
+end
